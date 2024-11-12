4fe01b462666d9a79a88cc4d39646667ce6f72d4
SovaaaQ <123616381+SovaaaQ@users.noreply.github.com>
2024-11-12 17:25:38 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title_1'>Конструктор почтовых ящиков</h1>
+        <h2 className='title_2'>Выберите модель почтового ящика</h2>
       </header>
     </div>
   );
