f8113510298ce4c41bdd881472ddb0a9d50131dc
SovaaaQ <123616381+SovaaaQ@users.noreply.github.com>
2024-11-12 17:48:01 +0500

    two

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import EntranceForm from './component/EntranceForm';
+import SizeMailbox from './component/SizeMailbox';
+import ChoiceModel from './component/ChoiceModel';
+import SpamAndBatteries from './component/SpamAndBatteries';
 
 function App() {
   return (
@@ -7,6 +11,12 @@ function App() {
         <h1 className='title_1'>Конструктор почтовых ящиков</h1>
         <h2 className='title_2'>Выберите модель почтового ящика</h2>
       </header>
+      <body>
+        <EntranceForm/>
+        <SizeMailbox/>
+        <ChoiceModel/>
+        <SpamAndBatteries/>
+      </body>
     </div>
   );
 }
