cf055c09f85a535e40e9291a6180df05ef5e3b89
sshkhndmd <hosslerjx@gmail.com>
2024-11-19 20:14:47 +0500

    seven

--- src/model/mailBoxModel.jsx
+++ src/model/mailBoxModel.jsx
@@ -4,17 +4,22 @@ import threeImage from "../img/three.svg"
 import fourImage from "../img/four.svg"
 import fiveImage from "../img/five.svg"
 import sixImage from "../img/six.svg"
+import eightImage from "../img/eight.svg"
+import nineImage from "../img/nine.svg"
+import tenImage from "../img/ten.svg"
+import elevenImage from "../img/eleven.svg"
 const mailBoxModel = [
     { value: "1", label: "Оптима Компакт", image: oneImage, description: "Оптима Компакт"},
     { value: "2", label: "Оптима", image: twoImage, description: "Оптима" },
     { value: "3", label: "Оптима Люкс", image: threeImage, description: "Оптима Люкс" },
-    { value: "4", label: "Оптима Авангард", image: fourImage, description: "Оптима Авангард" },
-    { value: "5", label: "Гарант", image: fiveImage, description: "Гарант" },
-    { value: "6", label: "Орион Престиж М", image: sixImage, description: "Орион Престиж М" },
-    { value: "7", label: "Омега", image: "" },
-    { value: "8", label: "Сигма", image: "" },
-    { value: "9", label: "Орион М", image: "" },
-    { value: "10", label: "Смарт", image: "" },
+    { value: "4", label: "Оптима Авангард", image: elevenImage, description: "Оптима Авангард" },
+    { value: "5", label: "Гарант", image: fourImage, description: "Гарант" },
+    { value: "6", label: "Гарант Люкс", image: fiveImage, description: "Гарант Люкс" },
+    { value: "7", label: "Орион М", image: sixImage, description: "Орион М" },
+    { value: "8", label: "Орион Престиж М", image: eightImage, description: "Орион Престиж М" },
+    { value: "9", label: "Омега", image: nineImage, description: "Омега" },
+    { value: "10", label: "Сигма", image: tenImage, description: "Сигма" },
+    { value: "11", label: "Смарт", image: elevenImage, description: "Смарт" }
 ];
 
 export default mailBoxModel;
