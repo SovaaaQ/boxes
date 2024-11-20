7a75b16225b79c55e95b4b0f27f1d159a298345e
PraporPingvin <sofasybbotina12@gmail.com>
2024-11-20 16:00:46 +0500

    eight

--- src/component/EntranceForm/EntranceForm.jsx
+++ src/component/EntranceForm/EntranceForm.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState } from 'react';
 import './EntranceForm.css';
 
@@ -116,4 +117,114 @@ const EntranceForm = () => {
   );
 };
 
+=======
+import React, { useState } from 'react'; 
+import './EntranceForm.css'; 
+ 
+const EntranceForm = () => { 
+  const [numEntrances, setNumEntrances] = useState(1); 
+  const [entrances, setEntrances] = useState([{ id: 1, numApartments: 0 }]); 
+  const [isChecked, setIsChecked] = useState(false); 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); 
+ 
+  const handleCheckboxChange = (event) => { 
+    const checked = event.target.checked; 
+    setIsChecked(checked); 
+ 
+    if (checked) { 
+      const sameApartments = entrances[0].numApartments; 
+      const newEntrances = entrances.map(entrance => ({ 
+        ...entrance, 
+        numApartments: sameApartments 
+      })); 
+      setEntrances(newEntrances); 
+    } 
+  }; 
+ 
+  const handleNumEntrancesChange = (event) => { 
+    const newNumEntrances = parseInt(event.target.value); 
+    setNumEntrances(newNumEntrances); 
+ 
+    if (newNumEntrances > entrances.length) { 
+      const newEntrances = [...entrances]; 
+      for (let i = entrances.length + 1; i <= newNumEntrances; i++) { 
+        newEntrances.push({ id: i, numApartments: 0 }); 
+      } 
+      setEntrances(newEntrances); 
+    } else if (newNumEntrances < entrances.length) { 
+      setEntrances(entrances.slice(0, newNumEntrances)); 
+    } 
+  }; 
+ 
+  const handleNumApartmentsChange = (event, index) => { 
+    const newNumApartments = parseInt(event.target.value); 
+    const newEntrances = [...entrances]; 
+    newEntrances[index].numApartments = newNumApartments; 
+ 
+    if (isChecked) { 
+      newEntrances.forEach(entrance => { 
+        entrance.numApartments = newNumApartments; 
+      }); 
+    } 
+ 
+    setEntrances(newEntrances); 
+  }; 
+ 
+  const handleButtonClick = () => { 
+    setIsDropdownOpen(!isDropdownOpen); 
+  }; 
+ 
+  return ( 
+    <div> 
+      <div className='container div_num_entrances'> 
+        <label className='num_entrances'> 
+          Количество подъездов 
+          <input type="number" className='num_entrances-input' value={numEntrances} onChange={handleNumEntrancesChange} /> 
+        </label> 
+        <div className='dropdown'> 
+          <button className='dropdown-button' onClick={handleButtonClick}> 
+            Количество квартир в подъездах 
+            <svg width="20" height="20" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+              <path d="M37 14.5L22 29.5L7 14.5" stroke="#333333" stroke-width="3" /> 
+            </svg> 
+          </button> 
+          {isDropdownOpen && ( 
+            <div className='dropdown-content'> 
+              <div className='button_choise'> 
+                <div className='label_apartment'>
+                  <label htmlFor="yes" className='label_text_num'>Одинаковое количество квартир</label> 
+                  <input 
+                    type="checkbox" 
+                    id="yes" 
+                    name="yes" 
+                    checked={isChecked} 
+                    onChange={handleCheckboxChange} 
+                  /> 
+                </div>
+ 
+                <div className={`entrance-grid ${entrances.length > 5 ? 'two-columns' : ''}`}> 
+                  {entrances.map((entrance, index) => ( 
+                    <div key={entrance.id} className='entrance-item'> 
+                      <div className='quntity_entrance'> 
+                        {entrance.id} подъезд 
+                        <input 
+                          type="number" 
+                          className='num_entrances-input' 
+                          value={entrance.numApartments} 
+                          onChange={(event) => handleNumApartmentsChange(event, index)} 
+                        /> 
+                      </div> 
+                    </div> 
+                  ))} 
+                </div> 
+              </div> 
+            </div> 
+          )} 
+        </div> 
+      </div> 
+    </div> 
+  ); 
+}; 
+ 
+>>>>>>> 2a04fee (eight)
 export default EntranceForm;
