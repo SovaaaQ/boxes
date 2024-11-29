7686da63bdbe4a236d35e0914d53e4aa1fce3af2
SovaaaQ <toxic_mari@mail.ru>
2024-11-29 11:14:53 +0500

    кнопки и цвет

--- src/component/ColorChoice/ColorChoice.jsx
+++ src/component/ColorChoice/ColorChoice.jsx
@@ -1,65 +1,90 @@
-import React, {useState} from 'react';
-import './ColorChoice.css';
+import React, { useState } from "react";
+import "./ColorChoice.css";
 
 const ColorChoice = () => {
-    const [isChecked, setIsChecked] = useState(false);
-    const [entrances, setEntrances] = useState([{id: 1, numApartments: 0}]);
-    const [isDropdown, setIsDropdown] = useState(false);
-    
-    const handleCheckboxChange = (event) => {
-        const checked = event.target.checked;
-        setIsChecked(checked);
-    
-        if (checked) {
-          const sameApartments = entrances[0].numApartments;
-          const newEntrances = entrances.map(entrance => ({
-            ...entrance,
-            numApartments: sameApartments
-          }));
-          setEntrances(newEntrances);
-        }
-      };
+  const [isDropdownBody, setIsDropdownBody] = useState(false);
+  const [isDropdownDoors, setIsDropdownDoors] = useState(false);
 
-    const ButtonClick = () => {
-        setIsDropdown(!isDropdown);
-    };
+  const toggleDropdownBody = () => {
+    setIsDropdownBody(!isDropdownBody);
+  };
 
-    return (
-        <div>
-            <div>
-               <label className='div_color'>
-                Цвет
-                </label> 
-                <div className='drop&down'>
-                    <button className='drop&down-button' onClick={ButtonClick}>
-                        Корпус
-                        <svg width="20" height="20" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M37 14.5L22 29.5L7 14.5" stroke="#333333" stroke-width="3"/>
-                        </svg>
-                    </button>
-                    {isDropdown && (
-                        <div className='dropdown-content'>
-                            <div className='button_choise'>
-                                <div className='label-frame'>
-                                    <label htmlFor="yes" className='label_text_num'>Цвет корпуса</label>
-                                </div>
-                            </div>
-                        </div>
-                    )}
+  const toggleDropdownDoors = () => {
+    setIsDropdownDoors(!isDropdownDoors);
+  };
+
+  return (
+    <div className="color-choice">
+      <div className="content-wrapper">
+        <label className="div_color">Цвет</label>
+        <div className="button-group">
+          <div className="drop">
+            <button className="dropdown-btn" onClick={toggleDropdownBody}>
+              Корпус
+              <span className="icon">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 44 44"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37 14.5L22 29.5L7 14.5"
+                    stroke="#333333"
+                    strokeWidth="3"
+                  />
+                </svg>
+              </span>
+            </button>
+            {isDropdownBody && (
+              <div className="dropdown-content">
+                <div className="button_choice">
+                  <div className="label-frame">
+                    <label htmlFor="yes" className="label_text_num">
+                      Цвет корпуса
+                    </label>
+                  </div>
                 </div>
-                <div className='drop&down'>
-                    <button className='drop&down-button' onClick={ButtonClick}>
-                        Дверцы
-                        <svg width="20" height="20" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M37 14.5L22 29.5L7 14.5" stroke="#333333" stroke-width="3"/>
-                        </svg>
-                    </button>
-                    
+              </div>
+            )}
+          </div>
+
+          <div className="drop">
+            <button className="dropdown-btn" onClick={toggleDropdownDoors}>
+              Дверцы
+              <span className="icon">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 44 44"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37 14.5L22 29.5L7 14.5"
+                    stroke="#333333"
+                    strokeWidth="3"
+                  />
+                </svg>
+              </span>
+            </button>
+            {isDropdownDoors && (
+              <div className="dropdown-content">
+                <div className="button_choice">
+                  <div className="label-frame">
+                    <label htmlFor="yes" className="label_text_num">
+                      Цвет дверцы
+                    </label>
+                  </div>
                 </div>
-            </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
 
 export default ColorChoice;
