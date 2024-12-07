fd8effc524d72a74b0d167fc56483609bdddb781
PraporPingvin <sofasybbotina12@gmail.com>
2024-12-07 09:23:39 +0500

    fix

--- src/component/EntranceForm/EntranceForm.jsx
+++ src/component/EntranceForm/EntranceForm.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './EntranceForm.css';
 import ButtonEntrance from '../ButtonEntrance/ButtonEntrance';
 
 const EntranceForm = () => {
   const [numEntrances, setNumEntrances] = useState(1);
-  const [entrances, setEntrances] = useState([{id: 1, numApartments: 0}]);
+  const [entrances, setEntrances] = useState([{ id: 1, numApartments: '' }]);
   const [isChecked, setIsChecked] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -29,7 +29,7 @@ const EntranceForm = () => {
     if (newNumEntrances > entrances.length) {
       const newEntrances = [...entrances];
       for (let i = entrances.length + 1; i <= newNumEntrances; i++) {
-        newEntrances.push({id: i, numApartments: 0});
+        newEntrances.push({ id: i, numApartments: '' });
       }
       setEntrances(newEntrances);
     } else if (newNumEntrances < entrances.length) {
@@ -38,7 +38,13 @@ const EntranceForm = () => {
   };
 
   const handleNumApartmentsChange = (event, index) => {
-    const newNumApartments = parseInt(event.target.value);
+    let newNumApartments = event.target.value;
+
+    // Remove leading zeros unless the value is "0"
+    if (newNumApartments.startsWith('0') && newNumApartments.length > 1) {
+      newNumApartments = newNumApartments.substring(1);
+    }
+
     const newEntrances = [...entrances];
     newEntrances[index].numApartments = newNumApartments;
 
@@ -61,13 +67,13 @@ const EntranceForm = () => {
         <label className='num_entrances'>
           Количество подъездов
           <input type="number" className='num_entrances-input' value={numEntrances}
-                 onChange={handleNumEntrancesChange}/>
+            onChange={handleNumEntrancesChange} />
         </label>
         <div className='dropdown'>
           <button className='dropdown-button' onClick={handleButtonClick}>
             Количество квартир в подъездах
             <svg width="20" height="20" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M37 14.5L22 29.5L7 14.5" stroke="#333333" stroke-width="3"/>
+              <path d="M37 14.5L22 29.5L7 14.5" stroke="#333333" stroke-width="3" />
             </svg>
           </button>
           {isDropdownOpen && (
@@ -86,11 +92,12 @@ const EntranceForm = () => {
 
                 <div className={`entrance-grid`}>
                   <div>
-                    {entrances.filter((x, i) => i < entrances.length / 2).map((entrance, index) => (
+                    {entrances.filter((x, i) => i < Math.ceil(entrances.length / 2)).map((entrance, index) => (
                       <div key={entrance.id} className='entrance-item'>
                         <div className='quntity_entrance'>
                           {entrance.id} подъезд
                           <input
+                            placeholder='0'
                             type="number"
                             className='num_entrances-input'
                             value={entrance.numApartments}
@@ -101,15 +108,16 @@ const EntranceForm = () => {
                     ))}
                   </div>
                   <div>
-                    {entrances.filter((x, i) => i >= entrances.length / 2).map((entrance, index) => (
+                    {entrances.filter((x, i) => i >= Math.ceil(entrances.length / 2)).map((entrance, index) => (
                       <div key={entrance.id} className='entrance-item'>
                         <div className='quntity_entrance'>
                           {entrance.id} подъезд
                           <input
+                            placeholder='0'
                             type="number"
                             className='num_entrances-input'
                             value={entrance.numApartments}
-                            onChange={(event) => handleNumApartmentsChange(event, index)}
+                            onChange={(event) => handleNumApartmentsChange(event, index + Math.ceil(entrances.length / 2))}
                           />
                         </div>
                       </div>
@@ -121,7 +129,7 @@ const EntranceForm = () => {
           )}
         </div>
       </div>
-      <ButtonEntrance numEntrances={numEntrances}/>
+      <ButtonEntrance numEntrances={numEntrances} />
     </div>
   );
 };
